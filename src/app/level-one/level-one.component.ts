5479d4b9882644d81082547329398e382a5d525b
joshgressman <joshgressman@gmail.com>
2017-08-05 13:25:54 -0500

    added basic game logic

--- src/app/level-one/level-one.component.ts
+++ src/app/level-one/level-one.component.ts
@@ -8,12 +8,42 @@ import { Component} from '@angular/core';
 
 export class LevelOneComponent {
    level1One = [{url: '../../assets/images/game/blackpanther.jpeg', points: 2.5, value: 'Black Panther'},
-                {url: '../../assets/images/game/blackwidow.jpeg', points: 2.5, value: 'Black Panther'},
-                {url: '../../assets/images/game/deadpool.jpeg', points: 2.5, value: 'Black Panther'},
-                {url: '../../assets/images/game/blackpanther.jpeg', points: 2.5, value: 'Black Panther'}];
+                {url: '../../assets/images/game/blackwidow.jpeg', points: 2.5, value: 'Black Widow'},
+                {url: '../../assets/images/game/deadpool.jpeg', points: 2.5, value: 'Deadpool'},
+                {url: '../../assets/images/game/hulk.jpeg', points: 2.5, value: 'Hulk'}];
 
+    level1Two = [{url: '../../assets/images/game/deadpool.jpeg', points: 2.5, value: 'Deadpool'},
+                 {url: '../../assets/images/game/hulk.jpeg', points: 2.5, value: 'Hulk'},
+                 {url: '../../assets/images/game/blackpanther.jpeg', points: 2.5, value: 'Black Panther'},
+                 {url: '../../assets/images/game/blackwidow.jpeg', points: 2.5, value: 'Black Widow'}
+                ];
+
+
+     show1: number;
+     show2: number;
+     points = 0;
+     value1: null;
+     value2: null;
 
   constructor() { }
+
+  onShow1(i: number, item){
+    this.show1 = i;
+    this.value1 = item.value;
+    if(this.value1 === this.value2){
+      this.points += 5;
+    }
+  }
+
+  onShow2(i: number, item){
+    this.show2 = i;
+    this.value2 = item.value
+    if(this.value1 === this.value2){
+           this.points += 5;
+    }
+  }
+
+
 
   //Going to need a method that generates random numbers, pulls the random numbers based on level and randomy places
   //them on the game board. maybe the *ngfor is not the best route though rather
